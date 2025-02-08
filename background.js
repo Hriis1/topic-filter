59a3c5c38370819b39309eacc319e5591a9a450c
hrishd <hrishd@abv.bg>
2025-02-08 17:08:39 +0200

    receive

--- background.js
+++ background.js
@@ -3,8 +3,19 @@
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Only act when the tab is completely loaded
-    if (changeInfo.status === 'complete' && tab.url && tab.url.includes("reddit.com")) {
-        console.log("This is a redit page");
+    if (changeInfo.status === 'complete' && tab.url) {
+        
+        if (tab.url.includes("reddit.com")) { //if its a reddit tab
+            chrome.tabs.sendMessage(tabId, {
+                type: "FILTER",
+                site: "reddit",
+            })
+                .then(() => {
+                    console.log("Message sent successfully to tab:", tabId);
+                })
+                .catch(err => {
+                    console.error("Message failed to send:", err);
+                });
+        }
     }
-  });
-  
+});
--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,11 @@
 //Main script that runs on the loaded page
 (() => {
-    
+    chrome.runtime.onMessage.addListener((commandProp, sender, response) => {
+        const { type, site } = commandProp;
+
+        if (type === "FILTER") {
+            console.log("Filter site: " + site);
+        }
+    });
+
 })();
