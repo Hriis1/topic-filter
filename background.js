de008c7a399942d8d94cea1270cd858ecef710b6
hrishd <hrishd@abv.bg>
2025-02-08 16:57:48 +0200

    log when its redit

--- background.js
+++ background.js
@@ -1,2 +1,10 @@
 //Background service worker
 //Runs independantly of the page
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    // Only act when the tab is completely loaded
+    if (changeInfo.status === 'complete' && tab.url && tab.url.includes("reddit.com")) {
+        console.log("This is a redit page");
+    }
+  });
+  
