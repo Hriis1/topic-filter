290965483db3f8fcb11d08267a126a43438e527f
hrishd <hrishd@abv.bg>
2025-02-09 10:12:54 +0200

    added a log for site that is not supproted

--- background.js
+++ background.js
@@ -4,18 +4,17 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Only act when the tab is completely loaded
     if (changeInfo.status === 'complete' && tab.url) {
-        
+
         if (tab.url.includes("reddit.com")) { //if its a reddit tab
             chrome.tabs.sendMessage(tabId, {
                 type: "FILTER",
                 site: "reddit",
-            })
-                .then(() => {
-                    console.log("Message sent successfully to tab:", tabId);
-                })
-                .catch(err => {
-                    console.error("Message failed to send:", err);
-                });
+            });
+        } else {
+            chrome.tabs.sendMessage(tabId, {
+                type: "DEFAULT",
+                site: "",
+            });
         }
     }
 });
